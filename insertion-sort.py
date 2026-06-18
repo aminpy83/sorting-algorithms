e562fd292b78bef73914cd31b3585a6bfc9499b8
aminpy83 <aminpyss@gmail.com>
2026-06-18 23:58:54 +0330

    insertion sort

---
+++ insertion-sort.py
@@ -0,0 +1,11 @@
+arr = [30, 123, 14, 1, 423, 0, -1, 239]
+
+for i in range(1, len(arr)):
+    key = arr[i]
+    j = i - 1
+
+    while j >= 0 and arr[j] > key:
+        arr[j + 1] = arr[j]
+        j -= 1
+
+    arr[j + 1] = key
